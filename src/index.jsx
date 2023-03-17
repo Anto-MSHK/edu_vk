1adc58ccb1b58f09e427c4fc0dc2e2133e7c4a48
YourecMart <Rulet.puzyaka@yandex.ru>
2023-03-18 02:36:03 +0300

    added redux toolkit

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
-
-import {GlobalProvider} from "./context"
+import { GlobalProvider } from "./context"
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 bridge.send("VKWebAppInit");
 
-ReactDOM.createRoot(document.getElementById("root")).render(<GlobalProvider children={<App />} />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GlobalProvider children={
+    <Provider store={store}>
+      <App />
+    </Provider>
+  } />
+);
 
 if (process.env.NODE_ENV === "development") {
   import("./eruda");
