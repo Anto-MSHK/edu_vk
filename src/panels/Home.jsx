d68fff55e5dca9b6ffe9df910404aea13d4e2a14
Rain <silverain1qw@gmail.com>
2023-03-18 18:08:47 +0300

    Mock file

--- src/panels/Home.jsx
+++ src/panels/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useRef} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import ReactDOM from "react-dom/client";
 import {
     Panel,
@@ -25,6 +25,39 @@ import {SkeletonAvatar, SkeletonText} from '../components/Skeleton';
 import {GlobalContext} from '../context';
 import {SimpleCard} from "../components/SimpleCard/SimpleCard.jsx";
 import {Icon24Dismiss} from "@vkontakte/icons";
+
+const mockChanges = [
+    {
+        title: 'Пары нет',
+        day: '19 марта',
+        tag: '4'
+    },
+    {
+        title: 'Пары нет',
+        day: '19 марта',
+        tag: '4'
+    },
+    {
+        title: 'Пары нет',
+        day: '19 марта',
+        tag: '4'
+    },
+    {
+        title: 'Пары нет',
+        day: '19 марта',
+        tag: '4'
+    },
+    {
+        title: 'Пары нет',
+        day: '19 марта',
+        tag: '4'
+    },
+    {
+        title: 'Пары нет',
+        day: '19 марта',
+        tag: '4'
+    },
+]
 
 
 const Home = ({id, fetchedUser}) => {
@@ -61,49 +94,48 @@ const Home = ({id, fetchedUser}) => {
                         {((user) => {
                             const userExists = !!user;
                             if (userExists)
-                                return <> <Cell
-                                    before={user.photo_200 ?
-                                        <Avatar
-                                                src={user.photo_200}/> : null}
-                                    onClick={() => window.open(`https://vk.com/id${user.id}`)}
-                                >
-                                    <div style={{display: 'flex', flexDirection: 'column', rowGap: 10}}>
-                                        {user?.first_name + " " + user?.last_name}
-                                    </div>
-                                </Cell>
-                                    <div style={{padding: 10}}>
-                                        <Button stretched size="l" mode="secondary"
-                                                onClick={() => go("shredule")}>
-                                            Открыть расписание
-                                        </Button>
-                                    </div>
-                                    <Group style={{padding: 10}}
-                                        header={<Header mode="secondary"
-                                                        style={{display: 'flex', flexDirection: 'row'}}
-                                                        indicator={
-                                                            <Counter size="s" mode="prominent">
-                                                                3
-                                                            </Counter>
-                                                        }>
-                                            Замены
-                                        </Header>}>
-                                        <Div>
-                                            <SimpleCard title={'Пары нет'} tag={'4'} day={'19 марта'} user={user}/>
-                                            <SimpleCard title={'Замена'} tag={'1'} day='21 марта' user={user}/>
-                                            <SimpleCard title={'Пары нет'} tag={'3'} day='25 марта' user={user}/>
-                                        </Div>
-                                    </Group>
-
-                                </>
-                            else
-                                return <Cell
-                                    disabled={true}
-                                    before={<SkeletonAvatar/>}
-                                    subtitle={<SkeletonText style={{height: 20, width: 140}}/>}
-                                >
-                                    <SkeletonText style={{height: 20, width: 60}}/>&nbsp; <SkeletonText
-                                    style={{height: 20, width: 100}}/>
-                                </Cell>
+                            return <> <Cell
+                                before={user.photo_200 ?
+                                    <Avatar
+                                        src={user.photo_200}/> : null}
+                                onClick={() => window.open(`https://vk.com/id${user.id}`)}
+                            >
+                                <div style={{display: 'flex', flexDirection: 'column', rowGap: 10}}>
+                                    {user?.first_name + " " + user?.last_name}
+                                </div>
+                            </Cell>
+                                <div style={{padding: 10}}>
+                                    <Button stretched size="l" mode="secondary"
+                                            onClick={() => go("shredule")}>
+                                        Открыть расписание
+                                    </Button>
+                                </div>
+                                <Group style={{padding: 10}}
+                                       header={<Header mode="secondary"
+                                                       style={{display: 'flex', flexDirection: 'row'}}
+                                                       indicator={
+                                                           <Counter size="s" mode="prominent">
+                                                               {mockChanges.length}
+                                                           </Counter>
+                                                       }>
+                                           Замены
+                                       </Header>}>
+                                    <Div>
+                                        {mockChanges && mockChanges.map((el, ) => {
+                                            return (<SimpleCard title={el.title} tag={el.tag} day={el.day}/>)
+                                        })}
+                                    </Div>
+                                </Group>
+                            </>
+                        else
+                            return <Cell
+                                disabled={true}
+                                before={<SkeletonAvatar/>}
+                                subtitle={<SkeletonText style={{height: 20, width: 140}}/>}
+                            >
+                                <SkeletonText style={{height: 20, width: 60}}/>&nbsp; <SkeletonText
+                                style={{height: 20, width: 100}}/>
+                            </Cell>
                         })(fetchedUser)}
                     </Group>
                 </div>
