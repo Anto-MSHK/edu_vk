397ce81f3ca3153c9d48de501f4e516c53e81a73
YourecMart <Rulet.puzyaka@yandex.ru>
2023-03-19 02:03:51 +0300

    response sort

--- src/store/services/scheduleService.js
+++ src/store/services/scheduleService.js
@@ -7,7 +7,21 @@ export const scheduleApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
     getScheduleByName: builder.query({
-      query: (name) => `/schedule/get`,
+      query: () => `/schedule/get`,
+      transformResponse: (response) => {
+        const sortedResponse = [...response]
+        sortedResponse.forEach(item => {
+          if (typeof item !== 'undefined') {
+            item.days.sort((a, b) => a.count - b.count)
+            item.days.forEach(day => {
+              day.lessons.sort((a, b) => a.count - b.count)
+            })
+          }
+        });
+        console.log('sorted')
+        console.log(sortedResponse)
+        return sortedResponse
+      },
     }),
   }),
 });
